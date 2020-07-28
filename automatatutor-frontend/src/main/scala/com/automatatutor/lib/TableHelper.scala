1315e9a1c57b6a68968e529a3d4257ed00c09a80
John Yenter-Briars <jyenterbriars@gmail.com>
2020-07-28 17:25:05 -0500

    Dont allow blank column headers to be clickable

--- automatatutor-frontend/src/main/scala/com/automatatutor/lib/TableHelper.scala
+++ automatatutor-frontend/src/main/scala/com/automatatutor/lib/TableHelper.scala
@@ -47,7 +47,8 @@ object TableHelper {
 
 		val headingsXml: NodeSeq = headings.zipWithIndex.map(heading_index => {
 			val onClickFunc = attributes.filter(_._1.equals("onClick")).head._2
-			<th style="cursor: pointer;" onClick={onClickFunc(heading_index._2, tableID)}>{ heading_index._1 } </th>
+			val onClickVal = if(heading_index._1.toString.isEmpty) "" else onClickFunc(heading_index._2, tableID)
+			<th style="cursor: pointer;" onClick={onClickVal}>{ heading_index._1 } </th>
 		})
 
 		<tr class={classString} style={styleString}> { headingsXml } </tr>
