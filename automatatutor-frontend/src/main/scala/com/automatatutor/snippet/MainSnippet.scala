a9b81faba474f5523d8953a79c5859389ef700cb
John Yenter-Briars <jyenterbriars@gmail.com>
2020-08-23 18:30:04 -0500

    Update pda word problem

--- automatatutor-frontend/src/main/scala/com/automatatutor/snippet/MainSnippet.scala
+++ automatatutor-frontend/src/main/scala/com/automatatutor/snippet/MainSnippet.scala
@@ -134,7 +134,7 @@ class Mainsnippet {
     val importLink = SHtml.link("/main/import", () => {}, Text("Import Problems"))
 
     val createProblemLink = SHtml.link("/main/problempool/create", () => {
-      val problemType = ProblemType.findByName("NFA to DFA")
+      val problemType = ProblemType.findByName("PDA Words")
       CurrentProblemTypeInCourse(problemType.head)
     }, Text("Create the problem"))
     if (user.isAdmin) return autogenButton ++ problemPoolButton ++ usersButton ++  Unparsed("&emsp;") ++ exportLink ++  Unparsed("&emsp;") ++ importLink ++ createProblemLink
